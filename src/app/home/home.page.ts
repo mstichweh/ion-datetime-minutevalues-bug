8aa6a1683027a49091ddc45dd85c8156808189d0
Mark Stichweh <mark.stichweh@rle.de>
2022-02-04 18:19:35 +0100

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild } from '@angular/core';
+import { DatetimeCustomEvent, IonModal } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild(IonModal)
+  public modal: IonModal;
+
+  public timeValue;
+
+  constructor() {}
+
+  public async openModal(): Promise<void> {
+    await this.modal.present();
+  }
+
+  public onChange(e: Event): void {
+    this.timeValue = (e as DatetimeCustomEvent).detail.value;
+  }
+}
